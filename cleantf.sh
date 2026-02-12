b2d312ddca8ec6b228dc4be340179f499d907797
ajulcamera-ctrl <ajul.camera@gmail.com>
2026-02-12 08:35:34 +0000

    Update cleantf.sh

--- cleantf.sh
+++ cleantf.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-rm -rf ~/.terraform
-rm -rf ~/aws-learn-train/**/.terraform
-rm -f  ~/aws-learn-train/**/.terraform.lock.hcl
-echo "Terraform cache cleaned"
+set -e
+
+BASE_DIR=~/aws-learn-train
+
+echo "Cleaning Terraform caches under $BASE_DIR"
+echo ""
+
+# Remove .terraform directories
+find "$BASE_DIR" -type d -name ".terraform" -prune -exec rm -rf {} +
+
+# Remove lock files
+find "$BASE_DIR" -type f -name ".terraform.lock.hcl" -delete
+
+echo ""
+echo "Terraform cleanup complete."
+echo ""
+
 df -kh ~
