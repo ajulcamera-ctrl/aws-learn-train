1156adeab7f73d9749b3aa9aedb66d80344d4647
ajulcamera-ctrl <ajul.camera@gmail.com>
2026-02-14 18:39:01 +0000

    Rename output and add new outputs for notes and certificate

--- day11/terraform/outputs.tf
+++ day11/terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "alb_dns" {
+output "alb_dns_name" {
   value = aws_lb.app.dns_name
+}
+
+output "notes_https_url" {
+  value = "https://notes.${var.domain_name}"
+}
+
+output "certificate_dns_validation_record" {
+  value = aws_acm_certificate.notes_cert.domain_validation_options
 }
