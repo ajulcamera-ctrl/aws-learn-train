f14bb923473bf0365f6a9926d7f738904d7c7fb8
ajulcamera-ctrl <ajul.camera@gmail.com>
2026-02-16 22:59:19 +0000

    Update test.sh

--- day12/test.sh
+++ day12/test.sh
@@ -10,4 +10,4 @@ echo "You can run the following command to see if the DB connection worked:"
 echo "ssh ec2-user@$EC2_IP 'cat /home/ec2-user/db_test.txt'"
 
 # Optional: direct run if SSH key is already set up
-# ssh -o StrictHostKeyChecking=no ec2-user@$EC2_IP 'cat /home/ec2-user/db_test.txt'
+ssh -o StrictHostKeyChecking=no ec2-user@$EC2_IP 'cat /home/ec2-user/db_test.txt'
