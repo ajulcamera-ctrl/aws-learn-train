b177898819dac4b15301b61598fc649cf6f7fda1
Ajul Shah <ajul.shah@gmail.com>
2026-03-06 19:02:40 +0000

    remove nonsense

--- infrastructure/days/day14/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "alb_dns" {
-  value = aws_lb.main.dns_name
-}
-
-output "ecr_repo" {
-  value = aws_ecr_repository.app.repository_url
-}
--- infrastructure/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-cd terraform
-terraform init
-terraform apply -auto-approve
